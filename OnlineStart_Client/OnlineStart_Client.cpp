118d2884122fb393eda1a99c5aa1627710021cfe
EomJaeWoong <zzang6535@gmail.com>
2016-07-13 00:19:49 +0900

    움직임 구현
    - 다중 접속시 움직임 달라지는 거 해결 해야됨
    - ID확인이 아닌 포인터로 자기 확인하게 만들기

--- OnlineStart_Client/OnlineStart_Client.cpp
+++ OnlineStart_Client/OnlineStart_Client.cpp
@@ -7,11 +7,25 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
+/*--------------------------------------------------------------------*/
+// 플레이어
+/*--------------------------------------------------------------------*/
 Star *g_Players[USER_MAX];
 int g_playerID;
+SOCKET sock;
 
-void DrawPlayer();
+/*--------------------------------------------------------------------*/
+// 함수
+/*--------------------------------------------------------------------*/
+BOOL KeyProcess();
+void Init();
+void Network();
+void Draw();
+void Disconnect();
 
+/*--------------------------------------------------------------------*/
+// 에러 표시
+/*--------------------------------------------------------------------*/
 void err_display(char *msg)
 {
 	LPVOID lpMsgBuf;
@@ -27,21 +41,41 @@ void err_display(char *msg)
 
 int main()
 {
+	Init();
+
+	while (1)
+	{
+		Network();
+		Draw();
+	}
+
+	return 0;
+}
+
+/*--------------------------------------------------------------------*/
+// 초기화
+// 네트워크, 콘솔 화면을 초기화 함
+/*--------------------------------------------------------------------*/
+void Init()
+{
 	int retval;
-	stPacket packet;
 
 	/*--------------------------------------------------------------------*/
 	// 윈속 초기화
 	/*--------------------------------------------------------------------*/
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return -1;
+		return;
 
 	/*--------------------------------------------------------------------*/
 	// 소켓 생성
 	/*--------------------------------------------------------------------*/
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == INVALID_SOCKET)	err_display("socket()");
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		err_display("socket()");
+		exit(1);
+	}
 
 	/*--------------------------------------------------------------------*/
 	// IP주소 셋팅
@@ -61,89 +95,186 @@ int main()
 	// 소켓 연결
 	/*--------------------------------------------------------------------*/
 	retval = connect(sock, (SOCKADDR *)&sockaddr, sizeof(sockaddr));
-	if (retval == SOCKET_ERROR)		err_display("connect()");
+	if (retval == SOCKET_ERROR)
+	{
+		err_display("connect()");
+		exit(1);
+	}
 
 	/*--------------------------------------------------------------------*/
 	// 콘솔 화면 초기화
 	/*--------------------------------------------------------------------*/
 	cs_Initial();
 	cs_ClearScreen();
+}
 
-	while (1)
+/*--------------------------------------------------------------------*/
+// 네트워크 처리
+/*--------------------------------------------------------------------*/
+void Network()
+{
+	int retval;
+
+	FD_SET Readset;
+	FD_ZERO(&Readset);
+	FD_SET(sock, &Readset);
+		
+	TIMEVAL Timeval;
+	Timeval.tv_sec = 0;
+	Timeval.tv_usec = 0;
+
+	retval = select(0, &Readset, NULL, NULL, &Timeval);
+
+	if (retval > 0)
 	{
-		Buffer_Clear();
-		retval = recv(sock, (char*)&packet, sizeof(packet), 0);
-
-		switch (packet.type)
+		if (FD_ISSET(sock, &Readset))
 		{
-		case 0 :
-			if (retval == SOCKET_ERROR)	err_display("No received ID\n\n");
-			
-			else if (retval == 0)	{}
-			else
-			{
-				if (g_Players[0] == NULL)
-				{
-					g_playerID = packet.ID;
-					g_Players[0] = new Star;
-					g_Players[0]->ID = g_playerID;
-					break;
-				}
+			stPacket packet;
+			retval = recv(sock, (char *)&packet, sizeof(packet), 0);
+			if (retval == SOCKET_ERROR){
+				err_display("recv()");
+				Disconnect();
 			}
-			break;
 
-		case 1 :
-			if (retval == SOCKET_ERROR)	err_display("No connection\n\n");
-
-			if (g_playerID != 0)
+			/*--------------------------------------------------------------------*/
+			// 패킷 처리
+			// 0 : ID할당		 (수신)
+			// 1 : 신규접속_자신 (수신)
+			// 2 : 신규접속_타인 (수신)
+			// 3 : 이동처리		 (수,발신)
+			/*--------------------------------------------------------------------*/
+			switch (packet.type)
 			{
-				if (g_Players[0]->ID == packet.ID)
+			case 0 :		
+				for (int iCnt = 0; iCnt < USER_MAX; iCnt++)
 				{
-					g_Players[0]->x = packet.x;
-					g_Players[0]->y = packet.y;
-				}
-
-				else{
-					for (int iCnt = 1; iCnt < USER_MAX; iCnt++)
+					if (g_Players[iCnt] == NULL)
 					{
-						if (g_Players[iCnt] == NULL)
-						{
-							g_Players[iCnt]->ID = packet.ID;
-							g_Players[iCnt]->x  = packet.x;
-							g_Players[iCnt]->y  = packet.y;
-						}
+						g_playerID = packet.ID;
+						g_Players[iCnt] = new Star;
+						g_Players[iCnt]->ID = g_playerID;
+						break;
 					}
 				}
+				break;
+
+			case 1 :
+				for (int iCnt = 0; iCnt < USER_MAX; iCnt++)
+				{
+					if (g_Players[iCnt] != NULL && g_Players[iCnt]->ID == packet.ID)
+					{
+						g_Players[iCnt]->x = packet.x;
+						g_Players[iCnt]->y = packet.y;
+					}
+				}
+				break;
+
+			case 2 :
+				Disconnect();
+				break;
+
+			case 3 :
+				for (int iCnt = 0; iCnt < USER_MAX; iCnt++)
+				{
+					if (g_Players[iCnt] != NULL && g_Players[iCnt]->ID == packet.ID)
+					{
+						g_Players[iCnt]->x = packet.x;
+						g_Players[iCnt]->y = packet.y;
+					}
+				}
+				break;
+
+			default:
+				break;
 			}
-			DrawPlayer();
-			Buffer_Flip();
-			break;
-
-		case 2 :
-			//접속해제
-			break;
-
-		case 3 :
-			//액션
-			//retval = send(sock, )
-			break;
-
-		default :
-			break;
 		}
 	}
 
-	closesocket(sock);
-	WSACleanup();
+	/*--------------------------------------------------------------------*/
+	// 키입력 패킷처리
+	/*--------------------------------------------------------------------*/
+	if (KeyProcess())
+	{
+		Star* pStar = NULL;
 
-	return 0;
+		for (int iCnt = 0; iCnt < USER_MAX; iCnt++)
+		{
+			if (g_Players[iCnt] != NULL && g_Players[iCnt]->ID == g_playerID)
+			{
+				pStar = g_Players[iCnt];
+				break;
+			}
+		}
+		
+		stPacket packet;
+		packet.type = 3;
+		packet.ID = pStar->ID;
+		packet.x = pStar->x;
+		packet.y = pStar->y;
+		
+		retval = send(sock, (char *)&packet, sizeof(packet), 0);
+		if (retval == SOCKET_ERROR){
+			err_display("send()");
+			Disconnect();
+		}
+	}
 }
 
-void DrawPlayer()
+/*--------------------------------------------------------------------*/
+// 화면 출력
+/*--------------------------------------------------------------------*/
+void Draw()
 {
+	Buffer_Clear();
 	for (int iCnt = 0; iCnt < USER_MAX; iCnt++)
 	{
 		if (g_Players[iCnt] != NULL && g_Players[iCnt]->ID != 0)
 			Sprite_Draw(g_Players[iCnt]->x, g_Players[iCnt]->y, '*');
 	}
+	
+	Buffer_Flip();
+	Sleep(50);
+}
+
+/*--------------------------------------------------------------------*/
+// 키입력 처리
+/*--------------------------------------------------------------------*/
+BOOL KeyProcess()
+{
+	int retval;
+	Star *pStar = NULL;
+
+	for (int iCnt = 0; iCnt < USER_MAX; iCnt++)
+	{
+		if (g_Players[iCnt] != NULL && g_Players[iCnt]->ID == g_playerID)
+		{
+			pStar = g_Players[iCnt];
+			break;
+		}
+	}
+	int x = pStar->x;
+	int y = pStar->y;
+
+	if (GetAsyncKeyState(VK_LEFT) & 0x8001)		x--;
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8001)	x++;
+	if (GetAsyncKeyState(VK_UP) & 0x8001)		y--;
+	if (GetAsyncKeyState(VK_DOWN) & 0x8001)		y++;
+
+	if (pStar != NULL && (pStar->x != x || pStar->y != y)){
+		pStar->x = x;
+		pStar->y = y;
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+/*--------------------------------------------------------------------*/
+// 연결 해제
+/*--------------------------------------------------------------------*/
+void Disconnect()
+{
+	closesocket(sock);
+	WSACleanup();
+	exit(1);
 }
